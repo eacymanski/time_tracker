1a2ac9c9d7b27d78652a71f075447db52cd5c878
Liz Cymanski <eacymanski@gmail.com>
2015-12-02 20:39:04 +0000

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'static_pages#home'
 
   get 'help' =>'static_pages#help'
@@ -9,4 +13,5 @@ Rails.application.routes.draw do
    delete 'logout' => 'sessions#destroy'
 
   resources :users
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112200818) do
+ActiveRecord::Schema.define(version: 20151119200958) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",      null: false
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20151112200818) do
     t.string   "email"
     t.string   "name"
     t.string   "password_digest"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
